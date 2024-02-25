d18f5e5a69a2bb36ee1ad82adddbe1ad9a6fcc76
kitheka-james <kithekajames15@gmail.com>
2024-02-25 23:04:36 +0300

    python commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# This Python code asks the user for their name, age, and location, and then prints out a personalized message.
+
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you from? ")
+
+print("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
